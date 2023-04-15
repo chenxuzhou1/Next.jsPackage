87aafd7231811f9066a7130c7dd5e20722630d7a
chenxuzhou1 <117387635+chenxuzhou1@users.noreply.github.com>
2023-04-15 20:03:16 +0100

    Mhomepage

--- pages/Mhomepage/index.js
+++ pages/Mhomepage/index.js
@@ -136,7 +136,7 @@ export async function getServerSideProps(context) {
   let cookies = req.headers.cookie
   cookies = cookie.parse(req.headers.cookie || '')
   console.log(cookies)
-  const response = await fetch('/api/Mhomepage', {
+  const response = await fetch('https://next-js-package-dhmy.vercel.app/Mhomepage', {
     headers: {
       'Cookie': req.headers.cookie
     }
